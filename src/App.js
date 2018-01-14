884672d3f9f800575def11ad185b7735118bdbb1
Felipe Pantoja <felipempantoja@gmail.com>
2018-01-14 16:18:59 -0200

    header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import 'normalize.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <nav>
+            <div className="row">
+              <img className="avatar" src="/img/avatar.png" alt="Avatar" />
+              <ul className="main-nav">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Work</a></li>
+                <li><a href="#">Personal Projects</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Social</a></li>
+              </ul>
+            </div>
+          </nav>
+          <div className="hero-text-box">
+            <h1>
+              Hi, I'm Felipe<br />
+              I'm a software developer based in Rio de Janeiro, Brazil<br />
+              Contact me. Any time.<br/>
+              THis portifolio is under construction. Thanks. 
+            </h1>
+            <a className="btn btn-full" href="#">Contact</a>
+            <a className="btn btn-ghost" href="#">More about me</a>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload...
-        </p>
       </div>
     );
   }
